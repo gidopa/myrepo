45fd0192583eff5b9b98eb9b022bcd0f5adddad9
DESKTOP-TSCK13O\605 <chanbum2466@gmail.com>
2023-12-29 10:19:16 +0900

    주석 수정

--- 05_조건문1/src/if_prac.java
+++ 05_조건문1/src/if_prac.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class if_prac {
 	public static void main(String[] args) {
-	Scanner scan = new Scanner(System.in);
-	
-	String Num = scan.nextLine();
-	int intNum = Integer.parseInt(Num);
-	if(intNum % 2 == 0 ) {
+		System.out.println("정수를 입력하세요");
+		Scanner scan = new Scanner(System.in);
+		
+	int num = scan.nextInt();
+	// String num = scan.nextLine();
+	// int rcv_num = Integer.Parseint(num);
+	if(num % 2 == 0 ) {
 		System.out.println("짝수");
 	}else {
 		System.out.println("홀수");
