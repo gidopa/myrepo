1878660f84bdea279329639343b9045147a3d8e3
DESKTOP-TSCK13O\605 <chanbum2466@gmail.com>
2024-01-02 16:32:41 +0900

    교육

---
+++ 07조건문_반복문_활용/src/Prac.java
@@ -0,0 +1,14 @@
+
+public class Prac {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		int i;
+		for(i=1;i<11;i++) {
+			sum += i;
+			System.out.println(i+"까지의 합 : "+sum);
+		}
+		System.out.println(i-1+"까지의 합 : "+sum);
+	}
+
+}
---
+++ 07조건문_반복문_활용/src/Prac1.java
@@ -0,0 +1,16 @@
+
+public class Prac1 {
+
+	public static void main(String[] args) {
+		long s_Time = System.currentTimeMillis();
+			for(int i=0;i<100000;i++) {
+				;
+			}
+		long e_Time = System.currentTimeMillis();
+		System.out.println(s_Time);
+		System.out.println(e_Time);
+	}
+	
+	
+
+}
