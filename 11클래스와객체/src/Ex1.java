eee7da268c892d576f2f40a74f46be6f4cab21a9
DESKTOP-TSCK13O\605 <chanbum2466@gmail.com>
2024-01-05 14:43:43 +0900

    주석 추가

--- 11클래스와객체/src/Ex1.java
+++ 11클래스와객체/src/Ex1.java
@@ -34,11 +34,11 @@ class FruitSeller {
 	int saleApple(int money) {        // buyApple 했는데 구매자가 돈이 없을때 어떻게 구현? -> 사전검사로 처리
 		int num = money / applePrice;
 		if(numOfApple - num < 0) {
-			System.out.println("가진 모든 사과 "+numOfApple+"개를 판매함");		// 가진 사과보다 더 많이 buy했을때 가진것만 판매하고 남은건 0으로 처리
-			int salesApple = numOfApple;
+			System.out.println("가진 모든 사과 "+numOfApple+"개를 판매함");		// 가진 사과보다 더 많이 구매하려 할 때 가진것만 판매하고 남은 갯수는 0으로 처리
+			int salesApple = numOfApple;					// numOfApple을 0으로 대입하기 위해 리턴값으로 0이 되기전 numOfApple 값 저장
 			myMoney += numOfApple * applePrice;				// 판 갯수만큼만 돈을 받음
 			numOfApple =0;
-			return salesApple;
+			return salesApple;								// 원래 가지고 있던 모든 사과의 갯수 리턴
 			
 		}else if(numOfApple - num >= 0){			// 일반적 경우 
 		numOfApple -= num;
@@ -52,9 +52,9 @@ class FruitSeller {
 		System.out.println("가진 돈 : " + myMoney);
 	}
 	
-	void takeApple(int money) {
-		int buying = money / wholesalePrice;
-		int totApplePrice = wholesalePrice * buying;
+	void getApple(int money) {
+		int buying = money / wholesalePrice;			//구매한 사과의 갯수 총 돈 / 구매 가격
+		int totApplePrice = wholesalePrice * buying;	//사과값 * 구매한 갯수  -> 실제 지불한가격
 		if(myMoney - totApplePrice <0) {
 			System.out.println("돈이 없음");
 		}else {
@@ -75,10 +75,10 @@ class FruitBuyer{
 		if (money > myMoney) {
 	        System.out.println("돈이 부족합니다.");
 	        return;
-	        }
-		int originNumOfApple = numOfApple;			// 구매자가 가진 돈보다 많이 사려고 하면 return;으로 메서드 종료
+	        }										// 구매자가 가진 돈보다 많이 사려고 하면 return;으로 메서드 종료
+		int originNumOfApple = numOfApple;			// saleApple로 구매자의 사과갯수를 바꾸기 때문에 최초 상태의 사과 갯수 저장
 		numOfApple += seller.saleApple(money);		// saleApple로 상황에 따른 사과 갯수 누적
-		if(seller.numOfApple == 0) {
+		if(seller.numOfApple == 0) {				// 판매자가 가진것보다 많은 양을 사려고 했을때 -> sellr의 남은 사과를 0으로 만들어줌
 			 
 			myMoney -= (numOfApple-originNumOfApple) * seller.applePrice;		// 판매자가 가진 양보다 많이 사려하면 판매자가 가진 양만큼만 구매
 			
@@ -99,13 +99,13 @@ class FruitBuyer{
 	int stealApple(FruitSeller seller, int n) {
 
 		if(seller.numOfApple - n < 0) {
-			System.out.println("판매자가 가진 모든 사과 "+seller.numOfApple+"개 훔침");
-			numOfApple += seller.numOfApple;
+			System.out.println("판매자가 가진 모든 사과 "+seller.numOfApple+"개 훔침");  // 훔치려는 사과가 판매자가 가진 사과보다 많을때
+			numOfApple += seller.numOfApple;									  // 판매자가 가진 사과갯수만큼 누적하고 판매자의 사과갯수를 0으로 만듦
 			seller.numOfApple = 0;
 			
 		}else {
 			numOfApple += n;
-			seller.numOfApple -= n;
+			seller.numOfApple -= n;					// 구매자가 훔친 n개 만큼 누적 판매자는 -
 		}
 		return numOfApple;
 	}
