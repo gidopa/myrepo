8b3037cb65f0f251175ec9432e06e3e5c966d0a6
DESKTOP-TSCK13O\605 <chanbum2466@gmail.com>
2024-01-12 16:12:31 +0900

    boj 공부

---
+++ 백준/src/백준/prob2562.java
@@ -0,0 +1,26 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class prob2562 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int[] arr = new int[9];
+		int max = 0;
+		int index= 0;
+		for(int i = 0;i<9;i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			if(arr[i]>max) {
+				max = arr[i];
+				index = i+1;
+			}		
+		}
+		sb.append(max).append("\n").append(index);
+		System.out.println(sb);
+	}
+
+}
