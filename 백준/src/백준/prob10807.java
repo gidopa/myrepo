60c645ca72cf1d1a8ad7800c28627d6aff5aaa64
DESKTOP-TSCK13O\605 <chanbum2466@gmail.com>
2024-01-12 12:41:21 +0900

    공부

---
+++ 백준/src/백준/prob10807.java
@@ -0,0 +1,30 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class prob10807 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st;
+		int[] arr = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=(Integer.parseInt(st.nextToken()));
+		}
+		int sum = 0;
+		int v = Integer.parseInt(br.readLine());
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]==v) {
+				sum ++;
+			}
+		}
+		System.out.println(sum);
+	}
+
+}
