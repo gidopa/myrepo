01b2bb3a2ebb807d350a4d3998adcdef2033178a
Gido <jdm2131@naver.com>
2024-01-28 21:29:20 +0900

    백준

--- 백준/src/심화1/prob1316.java
+++ 백준/src/심화1/prob1316.java
@@ -6,17 +6,25 @@ import java.io.InputStreamReader;
 
 public class prob1316 {
 
-	public static void main(String[] args)throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		;
 		int n = Integer.parseInt(br.readLine());
-		int cnt = 0 ;
-		for(int i=0;i<n;i++) {
+		int cnt = n;
+		for (int i = 0; i < n; i++) {
 			String str = br.readLine();
-			for(int j=0;j<str.length();j++) {
-				
-			}
-			
-		}
-	}
+			boolean[] arr = new boolean[26];
+			for (int j = 0; j < str.length(); j++) {
+				if (arr[str.charAt(j) - 'a'] == false) {
+					arr[str.charAt(j) - 'a'] = true;
+				} else if (j>0 && str.charAt(j) != str.charAt(j - 1)) {
+					cnt--;
+					break;
+				}
 
+			}
+
+		}
+		System.out.println(cnt);
+	}
 }
