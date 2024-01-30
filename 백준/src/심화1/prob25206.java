7f41d1c425a1e36714392d5fa4195af34f9c0d90
Gido <jdm2131@naver.com>
2024-01-30 22:51:49 +0900

    백준 문제 풀기

--- 백준/src/심화1/prob25206.java
+++ 백준/src/심화1/prob25206.java
@@ -12,25 +12,25 @@ public class prob25206 {
 		StringTokenizer st;
 		String[] gradeList = { "A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0", "F", "P" };
 		Double[] gradeScore = { 4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0, 0.0 };
-		Double sum = 0.0;	// 학점 * 평점
-		Double total = 0.0; // 학점의 합
+		Double gradeSum = 0.0;	// 학점 * 평점
+		Double creditTotal = 0.0; // 학점의 합
 		for (int i = 0; i < 20; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			String subject = st.nextToken();
-			Double grade = Double.parseDouble(st.nextToken());
-			String score = st.nextToken();
+			Double credit = Double.parseDouble(st.nextToken());
+			String grade = st.nextToken();
 			
 			for(int j=0;j<gradeList.length;j++) {
-				if(score.equals(gradeList[j])) {
-					sum += grade * gradeScore[j];
-					if(!score.equals(gradeList[9])) {
-						total += grade;
+				if(grade.equals(gradeList[j])) {
+					gradeSum += credit * gradeScore[j];
+					if(!grade.equals(gradeList[9])) {
+						creditTotal += credit;
 					}
 				}
 			}
 
 		}
-		System.out.println(sum/total);
+		System.out.println(gradeSum/creditTotal);
 
 	}
 
